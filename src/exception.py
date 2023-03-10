0e3dc9da5d3135ff836ed0ad531fdc4110c7f8c9
Jay Pandya <jpandya065@gmail.com>
2023-03-10 15:23:33 +0530

    EDA problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # it will gave on which line of code the error occurers and in which file 
